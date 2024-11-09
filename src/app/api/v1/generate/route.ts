4793d4ae7d9837e0c0acee6b9d1d24fb584d95bc
Ionfinisher <teddyassih@gmail.com>
2024-11-09 11:13:19 +0000

    finish prisma orm

--- src/app/api/v1/generate/route.ts
+++ src/app/api/v1/generate/route.ts
@@ -7,21 +7,16 @@ export async function POST(request: NextRequest): Promise<NextResponse> {
   const { question } = await request.json();
 
   try {
-    // const papers = await prisma.$queryRaw`
-    //   SELECT * FROM get_papers_by_keyword(${question})
-    // `;
+    const answer = await prisma.$queryRaw`
+    SELECT * FROM get_related_papers(${question});
+  `;
 
-    const papers = await prisma.$queryRaw`SELECT * FROM research_papers`;
-
-    console.log("Research Papers:", papers);
-    return NextResponse.json(papers, { status: 200 });
+    return NextResponse.json(answer);
   } catch (error) {
     console.error("Error fetching papers:", error);
     return NextResponse.json(
       { error: "Error fetching papers" },
       { status: 500 }
     );
-  } finally {
-    await prisma.$disconnect();
   }
 }
